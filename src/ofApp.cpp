c5879b6bcffddf3c8665654fdcebd3f4cf332a07
Nick Fox-Gieg <nick@fox-gieg.com>
2020-03-23 23:48:50 -0400

    basic test ok

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,23 +6,7 @@ void ofApp::setup() {
     height = ofGetHeight();
     
     checkerboard.load("textures/checkerboard.png");
-    
-    shaderName = "shaderExample";
-    
-    //#ifdef TARGET_OPENGLES
-        //shader1.load("shaders/" + shaderName + "GLES");
-    //#else
-        shader1.load(shaderName);
-    //#endif
-
-    // parameters for the shader
-    //shaderContrast = 0.8;//0.8;
-    //shaderBrightness = 0.4;
-    //shaderBlendMix = 0.5;
-    // 10 blend modes
-    //shaderBlendMode = 0;
-    doShader = false;
-    
+         
     fbo1.allocate(width, height, GL_RGBA);
     fbo2.allocate(width, height, GL_RGBA);
 
@@ -39,85 +23,32 @@ void ofApp::setup() {
     keystoneHandleSize = 50;
     keystoneHandleStroke = 10;
     
-    modeSelector = INTRO;
-    
-    editCounter = 0;
-    playCounter = 0;
-    swapCounter = -1;
-    
+    modeSelector = EDIT;
+       
     // old oF default is 96 - but this results in fonts looking larger than in other programs.
     ofTrueTypeFont::setGlobalDpi(72);
 
-    editFontSize = 30;
-    editLineHeight = 34.0f;
-    editLetterSpacing = 1.035;
-    editFont.load("fonts/verdana.ttf", editFontSize, true, true);
-    editFont.setLineHeight(editLineHeight);
-    editFont.setLetterSpacing(editLetterSpacing);
-    editLeftMargin = 90;
-    editTopMargin = 180;
+    bgColor = ofColor(0);
     
-    editFontColor = ofColor(225,225,225);
-    editFontHighlightColor = ofColor(255,255,0);
-    swapFontHighlightColor = ofColor(255,0,0);
-    playFontHighlightColor = ofColor(0,255,255);
-    editBgColor = ofColor(127,0,0);
+    fontSize = 72;
+    fontLineHeight = 68.0f;
+    fontLetterSpacing = 1.035;
+    fontSelector = 0;
+    fontSizeChangeIncrement = 4;
 
-    introBgColor = ofColor(60,60,120);
+	fontsList.push_back("fonts/verdana.ttf");
+	fontsList.push_back("fonts/vcr_osd_mono.ttf");
+	fontsList.push_back("fonts/arcade.ttf");
+    fontsList.push_back("fonts/arcade_classic.ttf");
+    fontsList.push_back("fonts/stitch_warrior.ttf");
     
-    playFontSize = 72;
-    playLineHeight = 68.0f;
-    playLetterSpacing = 1.035;
-    playFontSelector = 0;
-    playFontSizeChangeIncrement = 4;
-
-    playFontsList.push_back("fonts/vcr_osd_mono.ttf");
-    playFontsList.push_back("fonts/arcade.ttf");
-    playFontsList.push_back("fonts/arcade_classic.ttf");
-    playFontsList.push_back("fonts/stitch_warrior.ttf");
+    initFonts();   
+  
+    fontLeftMargin = 90;
+    fontTopMargin = (height/2) + (fontSize/2);
     
-    initFonts();
-    
-    playImagesList.push_back("textures/bowie.png");
-    playImagesList.push_back("textures/duckhunt.png");
-    playImagesList.push_back("textures/gnr.png");
-    playImagesList.push_back("textures/pacman.png");
-    playImagesList.push_back("textures/rambo.png");
-    playImagesList.push_back("textures/aliens.jpg");
-    playImagesList.push_back("textures/starfox.png");
-
-    initImages();
-    
-    playLeftMargin = 90;
-    playTopMargin = (height/2) + (playFontSize/2);
-    
-    playFontColor = ofColor(255,225,225);
-    playBgColor = ofColor(0,127,0);
-
-    editStr.push_back("");
-    playStr.push_back("");
-    
-    introStr.push_back("- ~ - EDIT - ~ -");
-    introStr.push_back("");
-    introStr.push_back("TAB:  go to Play mode");
-    introStr.push_back("Letters:  you know what to do");
-    introStr.push_back("Down arrow:  new singer");
-    introStr.push_back("Question mark:  select two singers to swap");
-    introStr.push_back("1, 2, 3, 4:  select keystone corner");
-    introStr.push_back("5:  reset keystoning");
-    introStr.push_back("");
-    introStr.push_back("");
-    introStr.push_back("~ - ~ PLAY ~ - ~");
-    introStr.push_back("");
-    introStr.push_back("TAB:  go to Edit mode");
-    introStr.push_back("SPACE:  next singer");
-    introStr.push_back("Z:  previous singer");
-    introStr.push_back("F:  change font");
-    introStr.push_back("Plus, Minus:  change font size");
-    introStr.push_back("B:  change background");
-    
-    playImageSelector = 0;
-    
+    fontColor = ofColor(255);
+    bgColor = ofColor(0);    
 }
 
 //--------------------------------------------------------------
@@ -125,43 +56,10 @@ void ofApp::update() {
     fbo1.begin();
     ofClear(255,255,255, 0);
     
-    if (modeSelector == INTRO) {
-        ofBackground(introBgColor);
-        ofSetColor(editFontColor);
-
-        for (int i=0; i<introStr.size(); i++) {
-            editFont.drawString(introStr[i], editLeftMargin, editTopMargin + (i * editLineHeight));
-        }
-    } else if (modeSelector == EDIT || modeSelector == SWAP) {
-        ofBackground(editBgColor);
-        for (int i=0; i<editStr.size(); i++) {
-            if (i == editCounter || i == swapCounter) {
-                if (i == editCounter && (modeSelector != SWAP || i != swapCounter)) {
-                    ofSetColor(editFontHighlightColor);
-                } else if (modeSelector == SWAP && i == swapCounter && swapCounter != -1) {
-                    ofSetColor(swapFontHighlightColor);
-                }
-            } else {
-                if (i == playCounter) {
-                    ofSetColor(playFontHighlightColor);
-                } else {
-                    ofSetColor(editFontColor);
-                }
-            }
-            editFont.drawString(ofToString(i+1) + ". " + editStr[i], editLeftMargin, editTopMargin + (i * editLineHeight));
-        }
-        
-    } else if (modeSelector == PLAY) {
-        if (playImages.size() > 0) {
-            playImages[playImageSelector].draw(0,0,width,height);
-            ofSetColor(0,127);
-            ofRectangle(0,0,width,height);
-        } else {
-            ofBackground(playBgColor);
-        }
-        ofSetColor(playFontColor);
-        playFonts[playFontSelector].drawString(playStr[0], playLeftMargin, playTopMargin);
-        
+    if (modeSelector == EDIT) {
+        ofBackground(bgColor);
+		ofSetColor(fontColor);
+        fonts[fontSelector].drawString(displayString, fontLeftMargin, fontTopMargin + fontLineHeight);     
     } else if (modeSelector == KEYSTONE) {
         ofSetColor(255);
         checkerboard.draw(0,0,width,height);
@@ -169,27 +67,12 @@ void ofApp::update() {
     ofSetColor(255); // why does this work?
     fbo1.end();
 
-    //ofTexture tex1 = fbo1.getTexture();
-
     fbo2.begin();
     ofClear(255,255,255, 0);
 
-    if (doShader) {
-        shader1.begin();
-        shader1.setUniformTexture("tex0", fbo1.getTexture(), 0);
-        shader1.setUniform1f("time", ofGetElapsedTimef());
-        shader1.setUniform2f("resolution", ofGetWidth(), ofGetHeight());
-        fbo1.getTexture().bind();
-        plane1.draw();
-        fbo1.getTexture().unbind();
-        shader1.end();
-
-        //shaderContrast += 0.01;
-    } else {
-        fbo1.getTexture().bind();
-        plane1.draw();
-        fbo1.getTexture().unbind();
-    }
+    fbo1.getTexture().bind();
+    plane1.draw();
+    fbo1.getTexture().unbind();
 
     if (modeSelector == KEYSTONE) {
         ofVec2f center = ofVec2f(plane1.getMesh().getVertex(keystoneIndex).x + plane1.getPosition().x, fbo2.getHeight() - (plane1.getMesh().getVertex(keystoneIndex).y + plane1.getPosition().y));
@@ -210,126 +93,23 @@ void ofApp::draw() {
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key) {
-    if (modeSelector == EDIT || modeSelector == PLAY) {
-        if (key == OF_KEY_TAB) {
-            if (modeSelector == EDIT) {
-                modeSelector = PLAY;
-            } else if (modeSelector == PLAY) {
-                modeSelector = EDIT;
-            } else if (modeSelector == SWAP) {
-                swapCounter = -1;
-                modeSelector = PLAY;
-            }
-        }
-        
-    }
-
-    if (modeSelector == INTRO) {
-        modeSelector = EDIT;
-    } else if (modeSelector == EDIT || modeSelector == SWAP) {
+void ofApp::keyPressed(int key) { 
+    if (modeSelector == EDIT) {
         if (key == OF_KEY_DEL || key == OF_KEY_BACKSPACE) {
-            if (modeSelector == EDIT) {
-                if (editStr[editCounter].length() > 0) {
-                    editStr[editCounter] = editStr[editCounter].substr(0, editStr[editCounter].length()-1);
-                } else if (editCounter != 0) {
-                    //editStr.erase(editStr.begin() + editCounter);
-                    //editCounter--;
-                    //if (editCounter < 0) editCounter = 0;
-                }
-            } else if (modeSelector == SWAP) {
-                if (swapCounter != -1) {
-                    if (editStr.size() > 1) {
-                        editStr.erase(editStr.begin() + swapCounter);
-                        if (editStr.size() > 1) {
-                            editCounter--;
-                        } else {
-                            editCounter = 0;
-                        }
-                
-                    } else if (editStr.size() == 1) {
-                        editStr[0] = "";
-                    }
-                }
-                swapCounter = -1;
-                modeSelector = EDIT;
-                if (editCounter <0) editCounter = 0;
+            if (displayString.length() > 0) {
+                displayString = displayString.substr(0, displayString.length()-1);
             }
-        } else if (key == '/' || key == '?') { //KeyControl()) {
-            if (modeSelector == EDIT) {
-                swapCounter = editCounter;
-                modeSelector = SWAP;
-            } else if (modeSelector == SWAP) {
-                //if (editCounter != swapCounter) {
-                if (swapCounter < 0) {
-                    swapCounter = 0;
-                } else if (swapCounter > editStr.size()-1) {
-                    swapCounter = editStr.size()-1;
-                }
-                string swapLine = editStr[swapCounter];
-                editStr[swapCounter] = editStr[editCounter];
-                editStr[editCounter] = swapLine;
-                //}
-                swapCounter = -1;
-                modeSelector = EDIT;
-            }
-        } else if (key == OF_KEY_UP && editCounter > 0) {
-            editCounter--;
-        } else if (key == OF_KEY_DOWN || key == OF_KEY_RETURN) {
-            //editCounter++;
-            if (editStr[editCounter].length() > 0) {
-                if (editCounter == editStr.size()-1) {
-                    editStr.push_back("");
-                    editCounter = editStr.size()-1;
-                } else {
-                    editCounter++;
-                }
-            } else if (editCounter < editStr.size()-1) {
-                editCounter++;
-            }
-        } else if (!keyIsArrow(key)) {
-            if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5') {
-                modeSelector = KEYSTONE;
-            } else {
-                if (editCounter < 0) {
-                    editCounter = 0;
-                } else if (editCounter > editStr.size()-1) {
-                    editCounter = editStr.size()-1;
-                }
-                if (key != OF_KEY_LEFT_SHIFT && key != OF_KEY_RIGHT_SHIFT && key != OF_KEY_CONTROL && key != OF_KEY_ALT) { // filter unwanted keys here
-                    editStr[editCounter].append(1, (char)key);
-                }
-            }
-        }
-        
-    } else if (modeSelector == PLAY) {
-        if (key == ' ') {
-            playCounter++;
-            if (playCounter > editStr.size()-1 || (editStr[playCounter].length() < 1 && playCounter == editStr.size()-1)) playCounter = 0;
-        } else if (key == 'z' || key == 'Z') {
-            playCounter--;
-            if (playCounter < 0) playCounter = editStr.size()-1;
-        } else if (key == 's' || key == 'S') {
-            doShader = !doShader;
-        } else if (key == 'f' || key == 'F') {
-            playFontSelector ++;
-            if (playFontSelector > playFonts.size()-1) playFontSelector = 0;
-        } else if (key == 'b' || key == 'B') {
-            playImageSelector ++;
-            if (playImageSelector > playImages.size()-1) playImageSelector = 0;
-        } else if (key == '-' || key == '_' || key == '+' || key == '=') {
-            if (key == '-' || key == '_') {
-                playFontSize -= playFontSizeChangeIncrement;
-                if (playFontSize < 10) playFontSize = 10;
-            } else if (key == '+' || key == '=') {
-                playFontSize += playFontSizeChangeIncrement;
-            }
-            
-            initFonts();
-        }
-        
-        if (editStr[playCounter].length() > 0) playStr[0] = editStr[playCounter];//ofSplitString(editStr[editCounter], "-");
-        
+		} else if (key == OF_KEY_RETURN) {
+			displayString = "";
+		} else if (keyIsNumber(key)) {
+			modeSelector = KEYSTONE;
+		} else if (keyIsArrow(key)) {
+			textStartPoint(key);
+		} else {
+			if (key != OF_KEY_LEFT_SHIFT && key != OF_KEY_RIGHT_SHIFT && key != OF_KEY_CONTROL && key != OF_KEY_ALT) { // filter unwanted keys here
+				displayString.append(1, (char)key);
+			}
+		}
     } else if (modeSelector == KEYSTONE) {
         if (key == '1') {
             keystoneIndex = 2;
@@ -350,53 +130,18 @@ void ofApp::keyPressed(int key) {
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-    
+void ofApp::textStartPoint(int key) {
+	if (key == OF_KEY_UP) {
+		fontTopMargin -= keystoneStep;
+	} else if (key == OF_KEY_DOWN) {
+		fontTopMargin += keystoneStep;
+	} else if (key == OF_KEY_LEFT) {
+		fontLeftMargin -= keystoneStep;
+	} else if (key == OF_KEY_RIGHT) {
+		fontLeftMargin += keystoneStep;
+	}
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ) {
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button) {
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::centerPlayText() {
-    if (playStr.size() > 0 && playCounter <= playStr.size()-1) {
-        playLeftMargin = (width/2) - ((playStr[playCounter].length() * playFontSize)/2);
-    }
-}
-
-//--------------------------------------------------------------
 void ofApp::keystoneVertex(int index, int key) {
     ofVec3f v = plane1.getMesh().getVertex(index);
     
@@ -413,7 +158,6 @@ void ofApp::keystoneVertex(int index, int key) {
     plane1.getMesh().setVertex(index, v);
 }
 
-
 void ofApp::saveKeystoneVertsOrig() {
     for (int i=0; i<plane1.getMesh().getVertices().size(); i++) {
         ofVec3f v = plane1.getMesh().getVertex(i);
@@ -429,21 +173,13 @@ void ofApp::loadKeystoneVertsOrig() {
 }
 
 void ofApp::initFonts() {
-    playFonts.clear();
-    for (int i=0; i<playFontsList.size(); i++) {
+   fonts.clear();
+    for (int i=0; i<fontsList.size(); i++) {
         ofTrueTypeFont font;
-        font.loadFont(playFontsList[i], playFontSize, true, true);
-        playFonts.push_back(font);
-        playFonts[i].setLineHeight(playLineHeight);
-        playFonts[i].setLetterSpacing(playLetterSpacing);
-    }
-}
-
-void ofApp::initImages() {
-    for (int i=0; i<playImagesList.size(); i++) {
-        ofImage image;
-        image.loadImage(playImagesList[i]);
-        playImages.push_back(image);
+        font.loadFont(fontsList[i], fontSize, true, true);
+        fonts.push_back(font);
+        fonts[i].setLineHeight(fontLineHeight);
+        fonts[i].setLetterSpacing(fontLetterSpacing);
     }
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -10,81 +10,34 @@ class ofApp : public ofBaseApp {
     
         void setup();
         void update();
-        void draw();
-        
+        void draw();       
         void keyPressed(int key);
-        void keyReleased(int key);
-        void mouseMoved(int x, int y );
-        void mouseDragged(int x, int y, int button);
-        void mousePressed(int x, int y, int button);
-        void mouseReleased(int x, int y, int button);
-        void windowResized(int w, int h);
-        void dragEvent(ofDragInfo dragInfo);
-        void gotMessage(ofMessage msg);
 
-        vector<string> editStr;
-        vector<string> playStr;
-        vector<string> introStr;
-    
-        enum ModeSelector { EDIT, PLAY, SWAP, KEYSTONE, INTRO };
+		string displayString = "";
+
+        enum ModeSelector { KEYSTONE, EDIT };
         ModeSelector modeSelector;
-    
-        // ~ ~ ~ edit / swap ~ ~ ~
-        ofTrueTypeFont editFont;
-        int editFontSize;
-        float editLetterSpacing;
-        float editLineHeight;
-        int editLeftMargin;
-        int editTopMargin;
-    
-        ofColor editFontColor;
-        ofColor editFontHighlightColor;
-        ofColor swapFontHighlightColor;
-        ofColor playFontHighlightColor;
-        ofColor editBgColor;
+       
+        ofColor bgColor = ofColor(0);
+		ofColor fontColor = ofColor(255);
 
-        int editCounter;
-        int swapCounter;
-
-        // ~ ~ ~ play ~ ~ ~
-        vector<string> playFontsList;
-        vector<ofTrueTypeFont> playFonts;
+        vector<string> fontsList;
+        vector<ofTrueTypeFont> fonts;
         void initFonts();
+        
+        int fontSelector;
     
-        vector<string> playImagesList;
-        vector<ofImage> playImages;
-        void initImages();
-    
-        int playFontSelector;
-        int playImageSelector;
-    
-        int playFontSize;
-        float playLetterSpacing;
-        float playLineHeight;
-        int playLeftMargin;
-        int playTopMargin;
-        void centerPlayText();
-        int playFontSizeChangeIncrement;
-    
-        ofColor playFontColor;
-        ofColor playBgColor;
-
-        ofColor introBgColor;
-    
-        int playCounter;
+        int fontSize;
+        float fontLetterSpacing;
+        float fontLineHeight;
+        int fontLeftMargin;
+        int fontTopMargin;
+        int fontSizeChangeIncrement;
     
         // ~ ~ ~ ~ ~ ~ ~
   
         ofImage checkerboard;
         ofFbo fbo1, fbo2;
-        ofShader shader1;
-        string shaderName;
-        string shaderTexName;
-        float shaderContrast;
-        float shaderBrightness;
-        float shaderBlendMix;
-        int shaderBlendMode;
-        bool doShader;
     
         ofPlanePrimitive plane1;
         int keystoneIndex;
@@ -100,5 +53,6 @@ class ofApp : public ofBaseApp {
         bool keyIsArrow(int key);
         bool keyIsNumber(int key);
 
+		void textStartPoint(int key);
 };
 
